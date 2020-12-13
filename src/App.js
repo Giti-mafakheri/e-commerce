16c5f39a71a15c246f8c2526d19af49f9ed3ee1b
giti-mafa <your@email.here.com>
2020-12-14 09:28:21 +1100

    item cart works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import data from "./data";
+import Product from "./components/Product";
 
 function App() {
   return (
@@ -10,18 +12,20 @@ function App() {
           </a>
         </div>
         <div>
-          <a href="/cart">Cart</a>
+          <a href="/Cart">Cart</a>
           <a href="/signin">Sign In</a>
         </div>
       </header>
 
       <main>
-        <div className="row center"></div>
+        <div className="row center">
+          {data.products.map((product) => (
+            <Product key={product._id} product={product}></Product>
+          ))}
+        </div>
       </main>
 
-      <footer>
-        <footer className="row center">All right reserved</footer>
-      </footer>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
