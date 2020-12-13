d413be8422b282934dd486987096e74a335f98e0
giti-mafa <your@email.here.com>
2020-12-13 23:57:01 +1100

    basic works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-<h1>home works</h1>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            e-commerce
+          </a>
+        </div>
+        <div>
+          <a href="/singin">singin</a>
+          <a href="/cart">cart</a>
+        </div>
+      </header>
+      <main>main</main>
+      <footer>
+        <footer className="row center">All right reserved</footer>
+      </footer>
     </div>
   );
 }
